a33f0e911a81b4bf8e60d81f4a6d45fccc71fa02
Subigya Lamichhane <subigyalamichhane@gmail.com>
2023-04-29 15:51:49 +0545

    made links open in new tab

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
+import autoAnimate from "@formkit/auto-animate";
 
 function createUUID() {
   // http://www.ietf.org/rfc/rfc4122.txt
@@ -25,10 +26,14 @@ function App() {
   const [messages, setMessages] = useState<
     { message: string; from: "us" | "them" }[]
   >([]);
+  const [fetched, setFetched] = useState(false);
   const messageDivRef = useRef<HTMLDivElement>(null);
   const bottomRef = useRef<HTMLDivElement>(null);
   // console.log(location.search);
   const queryParams = new URLSearchParams(location.search);
+  const parent = useRef(null);
+  const messageParent = useRef(null);
+
   // const backgroundColor = queryParams.get("backgroundColor") || "#fff";
 
   // const messageFieldColor = queryParams.get("messageFieldColor") || "#fff";
@@ -74,13 +79,14 @@ function App() {
   });
 
   const botId = queryParams.get("botId") || "2";
+  const botUuid = queryParams.get("botUuid");
   const [ID, setID] = useState(crypto.randomUUID());
 
   const getData = async () => {
     const response = await axios.get(
       "https://chat.witlingo.com/api/chat/" + botId + "/"
     );
-    console.log(response.data.data);
+    // console.log(response.data.data);
     return response.data.data;
   };
 
@@ -90,6 +96,7 @@ function App() {
     const response2 = await axios.get(
       "https://chat-dev.witlingo.com/api/chat/" + botId + "/"
     );
+
     // console.log(response2.data.data);
 
     const response = {
@@ -107,17 +114,21 @@ function App() {
     const data = getData();
     data.then((data) => {
       // console.log(data);
-      setMessageFieldColor(data.settings.messageFieldColor);
-      setIncommingMessageColor(data.settings.incommingMessageColor);
-      setIncommingMessageTextColor(data.settings.incommingMessageTextColor);
-      setOutgoingMessageColor(data.settings.outgoingMessageColor);
-      setOutgoingMessageTextColor(data.settings.outgoingMessageTextColor);
-      setMessageFieldTextColor(data.settings.messageFieldTextColor);
-      setBackgroundColor(data.settings.backgroundColor);
+      if (data.settings) {
+        setMessageFieldColor(data.settings.messageFieldColor);
+        setIncommingMessageColor(data.settings.incommingMessageColor);
+        setIncommingMessageTextColor(data.settings.incommingMessageTextColor);
+        setOutgoingMessageColor(data.settings.outgoingMessageColor);
+        setOutgoingMessageTextColor(data.settings.outgoingMessageTextColor);
+        setMessageFieldTextColor(data.settings.messageFieldTextColor);
+        setBackgroundColor(data.settings.backgroundColor);
+      }
     });
     const sidebarData = getSidebarData();
     sidebarData.then((data) => {
       setSidebarCustomization(data);
+      setFetched(true);
+      parent.current && autoAnimate(parent.current);
     });
   }, []);
 
@@ -125,6 +136,7 @@ function App() {
     return messages.map((message, index) => (
       <div
         key={index}
+        ref={messageParent}
         className={`flex items-center ${
           message.from === "us" ? "justify-end" : "justify-start"
         }`}
@@ -161,69 +173,87 @@ function App() {
       }}
       className="h-screen"
     >
-      <div className="flex">
-        <div
-          className="hidden md:block w-1/3"
-          style={{
-            backgroundColor: sidebarCustomization.background_color,
-          }}
-        >
-          <div className="flex flex-col items-center justify-center h-screen">
-            <div className=" h-1/4"></div>
-            <div className="flex flex-col items-center">
-              <img
-                className="w-1/2"
-                src={sidebarCustomization.logo}
-                alt="logo"
-              />
-            </div>
-            <div className="pb-16"></div>
-            <div className="">
-              <ul
-                className="p-2 list-disc"
-                style={{
-                  color: "#008ea4",
-                }}
-              >
-                {sidebarCustomization.links.map((link, index) => (
-                  <li className="flex-1 p-2" key={index}>
-                    <a
-                      key={index}
-                      className="text-left font-bold text-sm"
-                      style={{
-                        color: "#008ea4",
-                      }}
-                      href={link.url}
-                    >
-                      {link.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="flex-grow flex flex-col h-screen justify-end items-end">
+      <div className="flex" ref={parent}>
+        {fetched && (
           <div
-            className="md:hidden w-full h-20 flex justify-center items-center"
+            className="hidden md:block w-1/3 transition-all duration-500 ease-in-out"
+            // classNames={{
+            //   enter: "transform transition duration-[500ms] ease-in-out",
+            //   enterFrom: "translate-x-full",
+            //   enterTo: "translate-x-0",
+            //   leave: "transform transition duration-[500ms] ease-in-out",
+            //   leaveFrom: "translate-x-0",
+            //   leaveTo: "translate-x-full",
+            // }}
             style={{
               backgroundColor: sidebarCustomization.background_color,
             }}
           >
-            <img className="h-12" src={sidebarCustomization.logo} alt="logo" />
-          </div>
-          <div className="flex-grow flex flex-col w-full justify-end">
-            <div
-              // id="message-box"
-              ref={messageDivRef}
-              className="w-full p-4 overflow-y-auto overflow-x-hidden"
-            >
-              {renderMessages()}
-              <div ref={bottomRef} className="h-6"></div>
+            <div className="flex flex-col items-center justify-center h-screen">
+              <div className=" h-1/4"></div>
+              <div className="flex flex-col items-center">
+                <img
+                  className="w-1/2"
+                  src={sidebarCustomization.logo}
+                  alt="logo"
+                />
+              </div>
+              <div className="pb-16"></div>
+              <div className="">
+                <ul
+                  className="p-2 list-disc"
+                  style={{
+                    color: "#008ea4",
+                  }}
+                >
+                  {sidebarCustomization.links.map((link, index) => (
+                    <li className="flex-1 p-2" key={index}>
+                      <a
+                        key={index}
+                        className="text-left font-bold text-sm"
+                        style={{
+                          color: "#008ea4",
+                        }}
+                        href={link.url}
+                        target="_blank"
+                      >
+                        {link.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
+        )}
+        {fetched && (
+          <div className="flex-grow flex flex-col h-screen justify-end items-end">
+            {fetched && (
+              <div
+                className="md:hidden w-full h-20 flex justify-center items-center"
+                style={{
+                  backgroundColor: sidebarCustomization.background_color,
+                }}
+              >
+                <img
+                  className="h-12"
+                  src={sidebarCustomization.logo}
+                  alt="logo"
+                />
+              </div>
+            )}
+            <div className="flex-grow flex flex-col w-full justify-end">
+              <div
+                // id="message-box"
+                ref={messageDivRef}
+                className="w-full p-4 overflow-y-auto overflow-x-hidden"
+              >
+                {renderMessages()}
+                <div ref={bottomRef} className="h-6"></div>
+              </div>
+            </div>
 
-          {/* <div className="md:hidden p-4 w-full flex justify-between">
+            {/* <div className="md:hidden p-4 w-full flex justify-between">
             {sidebarCustomization.links.map((link, index) => (
               <div className="flex-1 flex justify-center p-2">
                 <a
@@ -239,96 +269,99 @@ function App() {
               </div>
             ))}
           </div> */}
-          <form
-            className="w-full"
-            onSubmit={async (e) => {
-              e.preventDefault();
-              if (message === "") return;
-              setMessages([
-                ...messages,
-                {
-                  message,
-                  from: "us",
-                },
-              ]);
-              bottomRef.current?.scrollIntoView({ behavior: "smooth" });
-              const prevMessage = message;
-              setMessage("");
-              const bodyFormData = new FormData();
-              bodyFormData.append("message", prevMessage);
-              const response = await axios.post(
-                `https://chat.witlingo.com/api/chat/${botId}/${ID}/`,
-                bodyFormData
-              );
-              console.log(response.data);
+            <form
+              className="w-full"
+              onSubmit={async (e) => {
+                e.preventDefault();
+                if (message === "") return;
+                setMessages([
+                  ...messages,
+                  {
+                    message,
+                    from: "us",
+                  },
+                ]);
+                messageParent.current && autoAnimate(messageParent.current);
+                bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+                const prevMessage = message;
+                setMessage("");
+                const bodyFormData = new FormData();
+                bodyFormData.append("message", prevMessage);
+                const response = await axios.post(
+                  `https://chat.witlingo.com/api/chat/${botId}/${ID}/`,
+                  bodyFormData
+                );
+                console.log(response.data);
 
-              setMessages([
-                ...messages,
-                {
-                  message: prevMessage,
-                  from: "us",
-                },
-                {
-                  message: response.data.answer,
-                  from: "them",
-                },
-              ]);
-              console.log(bottomRef.current);
-              bottomRef.current?.scrollIntoView();
-              //   handleMessageSubmit(message);
-            }}
-          >
-            <div className="flex">
-              <input
-                value={message}
-                placeholder="Type your message..."
-                onChange={(e) => setMessage(e.target.value)}
-                style={{
-                  backgroundColor: "#" + messageFieldColor,
-                  color: "#" + messageFieldTextColor,
-                }}
-                className="h-16 p-4 flex-grow rounded-none "
-              ></input>
-              <button
-                type="submit"
-                style={{
-                  backgroundColor: "#" + messageFieldColor,
-                  color: "#" + messageFieldTextColor,
-                }}
-                className="p-4"
-              >
-                <svg
-                  fill={"#" + backgroundColor}
-                  height="20px"
-                  width="20px"
-                  version="1.1"
-                  id="Layer_1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink"
-                  viewBox="0 0 512.662 512.662"
-                  xmlSpace="preserve"
+                setMessages([
+                  ...messages,
+                  {
+                    message: prevMessage,
+                    from: "us",
+                  },
+                  {
+                    message: response.data.answer,
+                    from: "them",
+                  },
+                ]);
+                messageParent.current && autoAnimate(messageParent.current);
+                // console.log(bottomRef.current);
+                bottomRef.current?.scrollIntoView();
+                //   handleMessageSubmit(message);
+              }}
+            >
+              <div className="flex">
+                <input
+                  value={message}
+                  placeholder="Type your message..."
+                  onChange={(e) => setMessage(e.target.value)}
+                  style={{
+                    backgroundColor: "#" + messageFieldColor,
+                    color: "#" + messageFieldTextColor,
+                  }}
+                  className="h-16 p-4 flex-grow rounded-none "
+                ></input>
+                <button
+                  type="submit"
+                  style={{
+                    backgroundColor: "#" + messageFieldColor,
+                    color: "#" + messageFieldTextColor,
+                  }}
+                  className="p-4"
                 >
-                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                  <g
-                    id="SVGRepo_tracerCarrier"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  ></g>
-                  <g id="SVGRepo_iconCarrier">
-                    {" "}
-                    <g>
+                  <svg
+                    fill={"#" + backgroundColor}
+                    height="20px"
+                    width="20px"
+                    version="1.1"
+                    id="Layer_1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 512.662 512.662"
+                    xmlSpace="preserve"
+                  >
+                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
                       {" "}
                       <g>
                         {" "}
-                        <path d="M505.021,5.868c-0.064-0.043-0.085-0.107-0.128-0.149c-0.128-0.107-0.256-0.128-0.384-0.235 c-1.131-0.981-2.475-1.621-3.797-2.325c-0.427-0.213-0.747-0.576-1.195-0.768c-0.064-0.021-0.107-0.021-0.149-0.043 c-0.469-0.192-0.853-0.533-1.323-0.704c-1.771-0.661-3.648-0.875-5.547-1.045c-0.576-0.043-1.131-0.299-1.707-0.299 c-2.475-0.021-4.971,0.384-7.403,1.259L14.055,172.225c-7.445,2.709-12.779,9.323-13.867,17.173 c-1.045,7.851,2.304,15.637,8.768,20.245l141.888,101.355l20.032,140.309c1.237,8.533,7.488,15.488,15.851,17.643 c1.749,0.448,3.541,0.661,5.291,0.661c6.592,0,12.971-3.072,17.045-8.533l50.347-67.093l132.032,113.237 c3.947,3.371,8.875,5.141,13.909,5.141c2.389,0,4.779-0.405,7.125-1.216c7.168-2.56,12.48-8.768,13.845-16.277l85.995-468.928 C513.725,18.262,510.738,10.71,505.021,5.868z M240.125,348.396l-1.536,2.219l-32.747,43.669l-12.395-86.827l185.109-160.448 L240.125,348.396z"></path>{" "}
+                        <g>
+                          {" "}
+                          <path d="M505.021,5.868c-0.064-0.043-0.085-0.107-0.128-0.149c-0.128-0.107-0.256-0.128-0.384-0.235 c-1.131-0.981-2.475-1.621-3.797-2.325c-0.427-0.213-0.747-0.576-1.195-0.768c-0.064-0.021-0.107-0.021-0.149-0.043 c-0.469-0.192-0.853-0.533-1.323-0.704c-1.771-0.661-3.648-0.875-5.547-1.045c-0.576-0.043-1.131-0.299-1.707-0.299 c-2.475-0.021-4.971,0.384-7.403,1.259L14.055,172.225c-7.445,2.709-12.779,9.323-13.867,17.173 c-1.045,7.851,2.304,15.637,8.768,20.245l141.888,101.355l20.032,140.309c1.237,8.533,7.488,15.488,15.851,17.643 c1.749,0.448,3.541,0.661,5.291,0.661c6.592,0,12.971-3.072,17.045-8.533l50.347-67.093l132.032,113.237 c3.947,3.371,8.875,5.141,13.909,5.141c2.389,0,4.779-0.405,7.125-1.216c7.168-2.56,12.48-8.768,13.845-16.277l85.995-468.928 C513.725,18.262,510.738,10.71,505.021,5.868z M240.125,348.396l-1.536,2.219l-32.747,43.669l-12.395-86.827l185.109-160.448 L240.125,348.396z"></path>{" "}
+                        </g>{" "}
                       </g>{" "}
-                    </g>{" "}
-                  </g>
-                </svg>
-              </button>
-            </div>
-          </form>
-        </div>
+                    </g>
+                  </svg>
+                </button>
+              </div>
+            </form>
+          </div>
+        )}
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,58 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            "-webkit-transform": "translateX(-200px)",
+            transform: "translateX(-200px)",
+          },
+          "100%": {
+            "-webkit-transform": "translateX(0px)",
+            transform: "translateX(0px)",
+          },
+        },
+
+        // TODO: added this out
+        "slide-out": {
+          "0%": {
+            "-webkit-transform": "translateX(0px)",
+            transform: "translateX(0px)",
+          },
+          "100%": {
+            "-webkit-transform": "translateX(-200px)",
+            transform: "translateX(-200px)",
+          },
+        },
+
+        "slide-fwd": {
+          "0%": {
+            "-webkit-transform": "translateZ(0px)",
+            transform: "translateZ(0px)",
+          },
+          "100%": {
+            "-webkit-transform": "translateZ(160px)",
+            transform: "translateZ(160px)",
+          },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.5s ease-out",
+        "slide-fwd":
+          "slide-fwd 0.45s cubic-bezier(0.250, 0.460, 0.450, 0.940) both",
+
+        // TODO: added this
+        "slide-out": "slide-out 0.5s ease-out",
+      },
+      transitionProperty: {
+        height: "height",
+      },
+    },
+    cursor: {
+      "zoom-in": "zoom-in",
+      pointer: "pointer",
+    },
   },
   plugins: [],
 };
